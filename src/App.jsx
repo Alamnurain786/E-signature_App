06b21db6781a47cba08b066f09d0025a557f3fea
Nurain Alam <alamnurain786@gmail.com>
2024-05-30 07:35:35 +0545

    UI Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
+import Title from "./components/Title";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const stylesheet = {
+    border: "none",
+    borderBottom: "1px dotted black",
+    outline: "none",
+    padding: ".35rem 0",
+  };
 
   return (
     <>
-      <div>
-        <h1>Hellow World</h1>
+      <div className="container text-center">
+        <Title classes={"subtile"} text={"Name"} />
+        <Title classes={"subtile"} text={"Date"} />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo
+          asperiores magni ad consequuntur, aspernatur ab esse, iure fugiat
+          corporis nam voluptatibus ea deserunt aperiam! Est placeat magni sint
+          beatae.
+        </p>
+
+        <footer
+          className="d-flex"
+          style={{
+            justifyContent: "space-around",
+            position: "relative",
+            top: "40vh",
+          }}
+        >
+          <input type="date" value={""} style={stylesheet} />
+          <input type="text" value={""} style={stylesheet} />
+        </footer>
       </div>
     </>
   );
